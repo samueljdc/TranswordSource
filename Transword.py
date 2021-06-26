4c1652434e51199611831161919e885edaa28947
goverfl0w <officialdeltadev@gmail.com>
2021-06-26 17:00:18 -0400

    Reverting for fix.
    There

---
+++ Transword.py
@@ -0,0 +1,126 @@
+# Imports required API libraries.
+import discord
+from discord import Intents, Status, Activity, ActivityType
+from discord.ext import commands
+from discord_slash import SlashCommand
+from dbl import DBLClient
+
+# Imports additional libraries used.
+from datetime import datetime
+from colorama import Fore, Back, Style, init, ansi
+from os import system
+from string import ascii_letters
+from random import choice
+
+class Transword:
+    """ Setup the bot information here. """
+
+    def __init__(self):
+        # Handle all terminal formatting.
+        init()
+        system("title Transword - Bot Terminal")
+
+        # print(f"{Fore.WHITE}{Back.BLUE}{Style.BRIGHT}")
+        print(ansi.clear_screen())
+
+        # Define the bot variables.
+        self.bot = commands.Bot(
+            intents = Intents.all(),
+            command_prefix = "".join(choice(ascii_letters) for _n in range(12)),
+            help_command = None
+        )
+        self.slash = SlashCommand(
+            self.bot,
+            override_type = True,
+            sync_commands = True
+        )
+        self.cogs = ["Utils", "Translation", "SlashAPI", "TopAPI"]
+
+        # Define Top.gg parameters.
+        self.token = open(".DBL", "r").read()
+        self.API = DBLClient(
+            self.bot,
+            self.token,
+            post_shard_count = True
+        )
+
+        print("""  ______                                              __
+ /_  __/________ _____  ______      ______  _________/ /
+  / / / ___/ __ `/ __ \/ ___/ | /| / / __ \/ ___/ __  /
+ / / / /  / /_/ / / / (__  )| |/ |/ / /_/ / /  / /_/ /
+/_/ /_/   \__,_/_/ /_/____/ |__/|__/\____/_/   \__,_/
+                                                        """)
+        print("".join("-" for i in range(56)))
+
+        self.load_cogs()
+
+    def colored(self, text: str):
+        """ Allow colors to help format the Python terminal text to ease eyes. """
+
+        colors = {
+            "ERROR": f"{Fore.WHITE}{Back.RED}{Style.BRIGHT}",
+            "INFO": f"{Fore.WHITE}{Back.YELLOW}{Style.BRIGHT}",
+            "END": f"{Fore.WHITE}{Back.BLACK}{Style.RESET_ALL}"
+        }
+
+        for color in colors:
+            text = text.replace(f"[[{color}]]", colors[color])
+
+        return text
+
+    def load_cogs(self):
+        """ Loads all of the cogs for the bot. """
+
+        # Check for the directory itself.
+        if __name__ == "__main__":
+            for cog in self.cogs:
+                cog_type = "cogs.api" if "API" in cog else "cogs.commands"
+
+                self.bot.load_extension(f"{cog_type}.{cog}")
+                print(self.colored(f"[[INFO]][MAIN][[END]] Command [[INFO]]{cog}[[END]] has been loaded."))
+        else:
+            pass
+
+# Run the bot here.
+transword = Transword()
+
+@transword.bot.event
+async def on_ready():
+    """ Give us internal bot details once it's online. """
+
+    # Some internal information variables being passed.
+    time = datetime.now().strftime("%b %d %Y %H:%M:%S")
+    servers = len(transword.bot.guilds)
+
+    prints = [
+        f"[[INFO]][MAIN][[END]] The bot is now online, passing details...",
+        f"[[END]]... Started: [[INFO]]{time}[[END]]",
+        f"[[END]]... Server Count: [[INFO]]{servers}[[END]]",
+        f"[[END]]... Discord.py Version: [[INFO]]{discord.__version__}[[END]]",
+        f"[[END]]... Slash Version: [[INFO]]1.0.9.4[[END]]"
+    ]
+
+    for line in prints:
+        print(transword.colored(line))
+
+    print(transword.colored("[[END]]" + "".join("-" for i in range(56))))
+
+    # Set up the custom status.
+    await transword.bot.change_presence(
+        status = Status.idle,
+        activity = Activity(
+            name = "for /help",
+            type = ActivityType.watching
+        )
+    )
+
+    # Send the Top.GG stats.
+    await transword.API.post_guild_count(
+        transword.API.guild_count
+    )
+
+transword.bot.run(
+    open(".TOKEN", "r").read(),
+    bot = True,
+    reconnect = True
+)
---
+++ cogs/api/PatreonAPI.py
@@ -0,0 +1,5 @@
+class PatreonAPI:
+    """ API for handling all HTTP feedback to Patreon. """
+
+    def __init__(self):
+        pass
